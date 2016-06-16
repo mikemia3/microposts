91f003959a5b4054fe577a5016b2a6796acf4a56
misaki <mikemia3@gmail.com>
2016-06-16 02:17:07 +0000

    error in static_pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615041118) do
+ActiveRecord::Schema.define(version: 20160616012437) do
+
+  create_table "migroposts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "migroposts", ["user_id", "created_at"], name: "index_migroposts_on_user_id_and_created_at"
+  add_index "migroposts", ["user_id"], name: "index_migroposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +29,10 @@ ActiveRecord::Schema.define(version: 20160615041118) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "region"
+    t.string   "profile"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
